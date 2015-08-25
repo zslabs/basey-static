3ea6f05dee9956e30c81bcd455a592679ea5ad59
Zach Schnackel <zach@boomtownroi.com>
2015-08-25 12:07:36 -0400

    update font location

--- gulp-tasks/copy.js
+++ gulp-tasks/copy.js
@@ -1,7 +1,7 @@
 module.exports = function(gulp, $, browserSync, reload, merge, paths, files) {
   return function() {
     gulp.src('bower_components/uikit/src/fonts/*')
-    .pipe($.changed(paths.fonts.build))
-    .pipe(gulp.dest(paths.fonts.build));
+    .pipe($.changed(paths.fonts))
+    .pipe(gulp.dest(paths.fonts));
   };
 };
